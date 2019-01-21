cdc9a1df2bb0f38a858e4afa71ece1bf62aa0d2e
yuhong <yu.hong.one@163.com>
2019-01-21 16:22:30 +0800

    重新提交文件

--- tp_adsdk/src/main/java/com/tianpeng/tp_adsdk/tpadmobsdk/ad/listener/ADMobGenSplashAdListener.java
+++
@@ -1,8 +0,0 @@
-package com.tianpeng.tp_adsdk.tpadmobsdk.ad.listener;
-
-/**
- * Created by YuHong on 2019/1/4 0004.
- */
-public interface ADMobGenSplashAdListener extends ADMobGenAdListener {
-    void onADExposure();
-}

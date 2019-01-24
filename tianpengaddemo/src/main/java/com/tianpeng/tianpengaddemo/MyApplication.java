3f00a13ec42b07bda473a19257095a527fcb3800
yuhong <yu.hong.one@163.com>
2019-01-24 11:50:55 +0800

    demo提交

--- tianpengaddemo/src/main/java/com/tianpeng/tianpengaddemo/MyApplication.java
+++ tianpengaddemo/src/main/java/com/tianpeng/tianpengaddemo/MyApplication.java
@@ -2,6 +2,7 @@ package com.tianpeng.tianpengaddemo;
 
 import android.app.Application;
 
+import com.tianpeng.tp_adsdk.tpadmobsdk.ad.constant.ADMobGenAdPlaforms;
 import com.tianpeng.tp_adsdk.tpadmobsdk.common.TPADMobSDK;
 
 
@@ -21,7 +22,13 @@ public class MyApplication extends Application {
         super.onCreate();
         // TODO: 2018/10/22 修改为自己的appId 测试id = 123
         // TODO: debug参数 默认传入false.
-        TPADMobSDK.instance().initSdk(getApplicationContext(), APP_ID, debug);
+        // TODO: 接入的广告渠道列表.
+        TPADMobSDK.instance().initSdk(getApplicationContext(), APP_ID, debug
+                , ADMobGenAdPlaforms.PLAFORM_GDT
+                , ADMobGenAdPlaforms.PLAFORM_BAIDU
+                , ADMobGenAdPlaforms.PLAFORM_TOUTIAO
+                , ADMobGenAdPlaforms.PLAFORM_YOUDAO
+        );
 
     }
 
